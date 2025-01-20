a74e130d76542b4d17d95443951662835e9c3ec5
Vũ Minh Đức <mduc152001@gmail.com>
2025-01-20 21:04:06 +0700

    update ws comment object

--- app/routes/ws_routes.py
+++ app/routes/ws_routes.py
@@ -19,14 +19,10 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
             # Start TikTokLive client for the username
             client = TikTokLiveClient(username)
 
-            room_str_id = str(client.room_id)
-            print(f"Room ID: {room_str_id}")
-
             @client.on(CommentEvent)
             async def on_comment(event: CommentEvent):
-                print(f"Comment: {event.to_json()}")
                 comment = {
-                    "room_id": room_str_id,
+                    "room_id": str(event.common.room_id),
                     "msg_id": str(event.common.msg_id),
                     "from_live_of_tiktok_id": event.at_user.unique_id,
                     "customer_user_id": str(event.user.id),
@@ -34,7 +30,7 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
                     "customer_name": event.user.nickname,
                     "comment": event.comment,
                     "profile_picture_url": event.user.avatar_thumb.url_list[0],
-                    "created_at": datetime.fromtimestamp(int(event.common.create_time / 1000))
+                    "created_at": datetime.fromtimestamp(int(event.common.create_time / 1000)).isoformat()
                 }
                 # Send comments to the WebSocket
                 try:
@@ -45,7 +41,7 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
             # Save the client
             clients[username] = client
             # Run the TikTokLive client asynchronously
-            asyncio.create_task(client.start())
+            asyncio.create_task(client.start(fetch_room_info = True))
 
         # Keep the WebSocket connection alive
         while True:
