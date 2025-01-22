0d3c484c6232783cb872bb25a4f117e8cc14e651
Vũ Minh Đức <mduc152001@gmail.com>
2025-01-22 09:33:10 +0700

    add from_live_of_tiktok_id

--- app/routes/ws_routes.py
+++ app/routes/ws_routes.py
@@ -7,15 +7,17 @@ import asyncio
 ws_router = APIRouter()
 
 # WebSocket handler
-async def handle_tiktok_live(websocket: WebSocket, username: str):
-    client = TikTokLiveClient(username)
+async def handle_tiktok_live(websocket: WebSocket, tiktok_id: str):
+    client = TikTokLiveClient(tiktok_id)
 
     @client.on(CommentEvent)
     async def on_comment(event: CommentEvent):
+        print(f'Received comment: {event.to_json()}')
+
         comment = {
             "room_id": str(event.common.room_id),
             "msg_id": str(event.common.msg_id),
-            "from_live_of_tiktok_id": event.at_user.unique_id,
+            "from_live_of_tiktok_id": tiktok_id,
             "customer_user_id": str(event.user.id),
             "customer_tiktok_id": event.user.unique_id,
             "customer_name": event.user.nickname,
@@ -34,7 +36,7 @@ async def handle_tiktok_live(websocket: WebSocket, username: str):
         while True:
             await asyncio.sleep(1)  # Keep WebSocket connection alive
     except WebSocketDisconnect:
-        print(f"WebSocket disconnected for username: {username}")
+        print(f"WebSocket disconnected for username: {tiktok_id}")
     finally:
         # Ensure the client is stopped
         await client.disconnect(close_client = True)
